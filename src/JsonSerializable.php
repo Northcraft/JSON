6ca02139e70a8f58e99c4bedd08f68258fa56ebe
Eugene Han <eugene.han@outlook.com>
2015-11-20 00:03:01 +0800

    Fixed comment

--- src/JsonSerializable.php
+++ src/JsonSerializable.php
@@ -17,8 +17,7 @@ if (interface_exists("\\JsonSerializable")) {
     {
         /**
          * Specify data which should be serialized to JSON
-         * @link http://php.net/manual/en/jsonserializable.jsonserialize.php
-         * @return mixed data which can be serialized by <b>json_encode</b>,
+         * @return mixed data which can be serialized by <b>Encoder::encode()</b>,
          * which is a value of any type other than a resource.
          */
         function jsonSerialize();
